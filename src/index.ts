c79d26b6766f0b271d0931c1733702a3710cd625
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2025-01-08 15:28:08 +0900

    fix: parse the node itself instead of the parent node

--- src/index.ts
+++ src/index.ts
@@ -6,71 +6,70 @@ import { visit } from "unist-util-visit";
 import { isText, isTextDirective } from "./utils.js";
 
 const remarkRubyDirective: Plugin<[], Root> = () => {
-	return (tree) => {
-		visit(tree, isTextDirective, (node, _i, parent) => {
-			if (node.name !== "ruby") return;
-			if (!parent) return;
-			if (node.children.length === 0) return;
-			if (!isText(node.children[0])) return;
+  return (tree) => {
+    visit(tree, isTextDirective, (node) => {
+      if (node.name !== "ruby") return;
+      if (node.children.length === 0) return;
+      if (!isText(node.children[0])) return;
 
-			const textNode = node.children[0];
-			let regex = new RegExp(/(\([^)]*\))|（[^）]*）/);
-			if (!regex.test(textNode.value)) return;
+      const textNode = node.children[0];
+      let regex = new RegExp(/(\([^)]*\))|（[^）]*）/);
+      if (!regex.test(textNode.value)) return;
 
-			regex = new RegExp(/^([^\s(（]+)([（(])([^）)]+)([）)])$/);
-			const matches = textNode.value.match(regex);
-			if (!matches) return;
+      regex = new RegExp(/^([^\s(（]+)([（(])([^）)]+)([）)])$/);
+      const matches = textNode.value.match(regex);
+      if (!matches) return;
 
-			const [_, value, open, ruby, close] = matches;
+      const [_, value, open, ruby, close] = matches;
 
-			parent.data = {
-				...parent.data,
-				hName: node.name,
-				hProperties: {
-					...node.attributes,
-				},
-				hChildren: [
-					{
-						type: "text",
-						value: value,
-					},
-					{
-						type: "element",
-						tagName: "rp",
-						properties: {},
-						children: [
-							{
-								type: "text",
-								value: open,
-							},
-						],
-					},
-					{
-						type: "element",
-						tagName: "rt",
-						properties: {},
-						children: [
-							{
-								type: "text",
-								value: ruby,
-							},
-						],
-					},
-					{
-						type: "element",
-						tagName: "rp",
-						properties: {},
-						children: [
-							{
-								type: "text",
-								value: close,
-							},
-						],
-					},
-				],
-			};
-		});
-	};
+      node.data = {
+        ...node.data,
+        hName: node.name,
+        hProperties: {
+          ...node.attributes,
+        },
+        hChildren: [
+          {
+            type: "text",
+            value: value,
+          },
+          {
+            type: "element",
+            tagName: "rp",
+            properties: {},
+            children: [
+              {
+                type: "text",
+                value: open,
+              },
+            ],
+          },
+          {
+            type: "element",
+            tagName: "rt",
+            properties: {},
+            children: [
+              {
+                type: "text",
+                value: ruby,
+              },
+            ],
+          },
+          {
+            type: "element",
+            tagName: "rp",
+            properties: {},
+            children: [
+              {
+                type: "text",
+                value: close,
+              },
+            ],
+          },
+        ],
+      };
+    });
+  };
 };
 
 export default remarkRubyDirective;
