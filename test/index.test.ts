b6bc5cca2b48a510e40a4ed6b02ba988200adcc7
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2025-01-09 13:00:02 +0900

    feat: add data-ruby attribute to ruby tags

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,42 +9,42 @@ import { unified } from "unified";
 import remarkRubyDirective from "../src/index.js";
 
 const normalizeHtml = (html: string) => {
-  return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
-    p1 ? "<" : ">"
-  );
+	return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
+		p1 ? "<" : ">",
+	);
 };
 
 const parseMarkdown = mock(async (markdown: string, debug = false) => {
-  const remarkProcessor = unified()
-    .use(remarkParse)
-    .use(remarkDirective)
-    .use(remarkRubyDirective)
-    .use(remarkRehype)
-    .use(rehypeStringify);
+	const remarkProcessor = unified()
+		.use(remarkParse)
+		.use(remarkDirective)
+		.use(remarkRubyDirective)
+		.use(remarkRehype)
+		.use(rehypeStringify);
 
-  if (debug) {
-    const remarkOutput = await remarkProcessor.run(
-      remarkProcessor.parse(markdown)
-    );
-    console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
-  }
+	if (debug) {
+		const remarkOutput = await remarkProcessor.run(
+			remarkProcessor.parse(markdown),
+		);
+		console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
+	}
 
-  const output = String(await remarkProcessor.process(markdown));
+	const output = String(await remarkProcessor.process(markdown));
 
-  if (debug) {
-    console.log(
-      `HTML output:
-      ${normalizeHtml(output)}`
-    );
-  }
+	if (debug) {
+		console.log(
+			`HTML output:
+      ${normalizeHtml(output)}`,
+		);
+	}
 
-  return output;
+	return output;
 });
 
 describe("Test the basic usage", () => {
-  test("Ruby with half-width parentheses", async () => {
-    const input = ":ruby[とある科学の超電磁砲(レールガン)]";
-    const output = `
+	test("Ruby with half-width parentheses", async () => {
+		const input = ":ruby[とある科学の超電磁砲(レールガン)]";
+		const output = `
 		<p>
 			<ruby data-ruby="レールガン">
 				とある科学の超電磁砲
@@ -55,29 +55,29 @@ describe("Test the basic usage", () => {
 		</p>
     `;
 
-    const html = await parseMarkdown(input);
+		const html = await parseMarkdown(input);
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 
-  test("Ruby with multiple half-width parentheses", async () => {
-    const input =
-      ":ruby[とある科学の超電磁砲(レールガン)ととある魔術の禁書目録(インデックス)]";
-    // The first regex does not match if multiple parentheses exist and thus it returns halfway through.
-    const output = `
+	test("Ruby with multiple half-width parentheses", async () => {
+		const input =
+			":ruby[とある科学の超電磁砲(レールガン)ととある魔術の禁書目録(インデックス)]";
+		// The first regex does not match if multiple parentheses exist and thus it returns halfway through.
+		const output = `
     <p>
       <div>とある科学の超電磁砲(レールガン)ととある魔術の禁書目録(インデックス)</div>
     </p>
     `;
 
-    const html = await parseMarkdown(input);
+		const html = await parseMarkdown(input);
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 
-  test("Ruby with full-width parentheses", async () => {
-    const input = ":ruby[とある科学の超電磁砲（レールガン）]";
-    const output = `
+	test("Ruby with full-width parentheses", async () => {
+		const input = ":ruby[とある科学の超電磁砲（レールガン）]";
+		const output = `
 		<p>
 			<ruby data-ruby="レールガン">
 				とある科学の超電磁砲
@@ -88,23 +88,23 @@ describe("Test the basic usage", () => {
 		</p>
     `;
 
-    const html = await parseMarkdown(input);
+		const html = await parseMarkdown(input);
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 
-  test("Ruby with multiple full-width parentheses", async () => {
-    const input =
-      ":ruby[とある科学の超電磁砲（レールガン）ととある魔術の禁書目録（インデックス）]";
-    // The first regex does not match if multiple parentheses exist and thus it returns halfway through.
-    const output = `
+	test("Ruby with multiple full-width parentheses", async () => {
+		const input =
+			":ruby[とある科学の超電磁砲（レールガン）ととある魔術の禁書目録（インデックス）]";
+		// The first regex does not match if multiple parentheses exist and thus it returns halfway through.
+		const output = `
     <p>
       <div>とある科学の超電磁砲（レールガン）ととある魔術の禁書目録（インデックス）</div>
     </p>
     `;
 
-    const html = await parseMarkdown(input);
+		const html = await parseMarkdown(input);
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 });
